ea9fb91346a3492ade13e6951ac8b5052058ef36
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-06-07 00:57:12 -0500

    Update

--- mnist/convolutional.py
+++ mnist/convolutional.py
@@ -379,7 +379,7 @@ if __name__ == '__main__':
         
         FLAGS = tf.app.flags.FLAGS
 
-        tf.app.run(main=main, argv=[sys.argv[0], FLAGS.use_fp16, FLAGS.self_test, FLAGS.index, FLAGS.BATCH_SIZE, FLAGS.base_learning_rate, FLAGS.learning_dacay])
+        tf.app.run(main=main, argv=[sys.argv[:]])
         
         index += 1
         init_learning_dacay -= 0.1
